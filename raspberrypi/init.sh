03c5de31c3114a510c2f8d049415c149a5fba710
Martin Brochhaus <mbrochh@gmail.com>
2013-07-11 04:38:59 +0000

    More raspberry stuff / removed vim-markdown

--- raspberrypi/init.sh
+++ raspberrypi/init.sh
@@ -2,7 +2,11 @@ sudo apt-get update
 sudo apt-get upgrade
 
 # Install useful software
-sudo apt-get install zsh tmux
+sudo apt-get install python-setuptools python-dev zsh tmux polipo tor irssi screen socat libevent-dev libssl-dev ncurses-dev
+
+# Install pip & mercurial
+sudo easy_install pip
+sudo pip install mercurial
 
 # Install oh-my-zsh
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
@@ -14,3 +18,14 @@ ln -s mbrochh-dotfiles/.gitignore_global .
 ln -s mbrochh-dotfiles/.screenrc .
 ln -s mbrochh-dotfiles/raspberrypi/.tmux.conf .
 ln -s mbrochh-dotfiles/.bash_aliases .
+
+# install vim
+mkdir -p ~/src
+cd ~/src
+hg clone https://vim.googlecode.com/hg vim
+cd vim/src
+./configure --enable-pythoninterp --with-features=huge --prefix=$HOME/opt/vim
+make && mskr install
+mkdir -p ~/bin
+cd ~/bin
+ln -s $HOME/opt/vim/bin/vim
